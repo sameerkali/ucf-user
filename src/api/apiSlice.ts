10fb6feee4f706922166530e5d8cc1720fb7c574
Sameer Faridi <sameer@byte-digital.com.au>
2025-08-18 11:00:11 +0530

    remove unused API slices and related code; implement axios instance for API requests

--- src/api/apiSlice.ts
+++
@@ -1,8 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-export const apiSlice = createApi({
-  reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
-  tagTypes: ['Post'],
-  endpoints: () => ({}),
-});
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { apiSlice } from '../api/apiSlice';
 
 export const store = configureStore({
   reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
