fa7b72034095ec5aa151b10b361cd777d5602eb2
sameerkali <s.faridi007@gmail.com>
2025-09-22 22:52:29 +0530

    Enhance DemandCategories component: implement API data fetching with error handling, dynamic category icons, and order creation functionality; update form handling and improve user feedback.

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "https://ufc-backend-seven.vercel.app/";
+// export const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "https://ufc-backend-seven.vercel.app/";
 
-// export const BASE_URL = "https://vv5789qf-5000.inc1.devtunnels.ms/"
+export const BASE_URL = "https://vv5789qf-5000.inc1.devtunnels.ms/"
