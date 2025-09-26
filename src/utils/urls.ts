f56e312b34800a9d0c00440586530e938d415afd
sameerkali <s.faridi007@gmail.com>
2025-09-27 00:30:15 +0530

    fix the url

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://ufc-backend-seven.vercel.app/";
+export const BASE_URL = "https://ufc-backend-seven.vercel.app/";
 
-export const BASE_URL = "https://vv5789qf-5000.inc1.devtunnels.ms/"
+// export const BASE_URL = "https://vv5789qf-5000.inc1.devtunnels.ms/"
